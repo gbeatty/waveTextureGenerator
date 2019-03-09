ea8b4d1c18f4ff353ec4986053d51214c37d2ba0
Greg Beatty <greg.beatty@gmail.com>
2019-03-08 23:39:06 -0500

    updated inputs. Fixed output video frame rate

--- WaveTextureGenerator.cpp
+++ WaveTextureGenerator.cpp
@@ -18,20 +18,28 @@ int main()
     filesystem::remove_all(outputDir);
     filesystem::create_directory(outputDir);
 
-    const unsigned size = 1024;
     std::atomic<int> count = 0;
+	std::mutex mutex;
+
+	const unsigned size = 1024;
+	const int period = 20;
+	const int samplesPerSecond = 10;
+	const int numberOfSamples = period * samplesPerSecond;
 
 #ifdef _DEBUG
-    for (int i = 0; i < 200; ++i)
+    for (int i = 0; i < numberOfSamples; ++i)
 #else
-    Concurrency::parallel_for(0, 20, [&size, &count, &outputDir](int i)
+    Concurrency::parallel_for(0, numberOfSamples, [&](int i)
 #endif
     {
-        const double time = i;
+		const double timePerSample = 1.0 / static_cast<double>(samplesPerSecond);
+        const double time = i * timePerSample;
 
+		std::unique_lock<std::mutex> lock(mutex);
         std::cout << "count = " << count++ << std::endl;
+		lock.unlock();
         
-        tessendorf tess(0.000001, 20, Cartesian3(0.0, -1.0, 0.0), .02, time / 4.0, 50.0, size, size, 4000, 4000, .1, 1.0);
+        tessendorf tess(0.000001, 20, Cartesian3(0.0, -1.0, 0.0), .02, time, period, size, size, 4000, 4000, .1, 1.0);
 
         std::vector<VertexData> vertices = tess.simulate();
 
@@ -53,11 +61,11 @@ int main()
         }
 
         std::stringstream heightfieldFileName;
-        heightfieldFileName << "HeightField_" << std::setfill('0') << std::setw(3) << time << ".png";
+        heightfieldFileName << "HeightField_" << std::setfill('0') << std::setw(3) << i << ".png";
         auto heightfieldPath = outputDir / heightfieldFileName.str();
 
         std::stringstream normalFileName;
-        normalFileName << "NormalVector_" << std::setfill('0') << std::setw(3) << time << ".png";
+        normalFileName << "NormalVector_" << std::setfill('0') << std::setw(3) << i << ".png";
         auto normalPath = outputDir / normalFileName.str();
 
         stbi_write_png(heightfieldPath.string().c_str(), size, size, 1, &heightFieldPixels[0], 0);
@@ -66,16 +74,16 @@ int main()
 #ifndef _DEBUG
     );
 #endif
-
+	
     auto ffmpegPath = filesystem::current_path() / "ffmpeg.exe";
 
     std::stringstream heightfieldCmd;
-    heightfieldCmd << ffmpegPath.string() << " -y -start_number 0 -i ./WaveTextures/HeightField_%03d.png -c:v libx264 -vf \"fps = 20, format = yuv420p\" HeightField.mp4";
+    heightfieldCmd << ffmpegPath.string() << " -y -start_number 0 -framerate " << samplesPerSecond << " -i ./WaveTextures/HeightField_%03d.png -c:v libx264 -vf \"fps = " << samplesPerSecond << ", format = yuv420p\" HeightField.mp4";
     std::cout << heightfieldCmd.str() << std::endl;
     std::system(heightfieldCmd.str().c_str());
 
     std::stringstream normalVectorCmd;
-    normalVectorCmd << ffmpegPath.string() << " -y -start_number 0 -i ./WaveTextures/NormalVector_%03d.png -c:v libx264 -vf \"fps = 20, format = yuv420p\" NormalVector.mp4";
+    normalVectorCmd << ffmpegPath.string() << " -y -start_number 0 -framerate " << samplesPerSecond << " -i ./WaveTextures/NormalVector_%03d.png -c:v libx264 -vf \"fps = " << samplesPerSecond << ", format = yuv420p\" NormalVector.mp4";
     std::system(normalVectorCmd.str().c_str());
 
     return 0;
